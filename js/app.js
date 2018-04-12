44319e43bc24ff77139d9d7605de03833fbca0d8
Kevin Hwangpo <hwangpo3@gmail.com>
2018-04-12 14:33:50 -0700

    finished Tuesday's constructors

--- js/app.js
+++ js/app.js
@@ -1,292 +1,82 @@
 'use strict';
 
-var firstAndPikeShop = {
-  name: '1st and Pike',
-  minHourlyCustomers: 23,
-  maxHourlyCustomers: 65,
-  avgCookiePerSale: 6.3,
-  totalCookiesSold: 0,
-  cookiesSoldInformationByHour: [],
-  buildMarketSales: function() {
-    var openHour = 6;
-    var closeHour = 20;
-    for (var i = openHour; i <= closeHour; i++) { // loop through the hours
-      var morningEvening = 'pm: ';
-      if (i < 12) {
-        morningEvening = 'am: ';
-      }
+function SalmonCookies(name, minHourlyCustomers, maxHourlyCustomers, avgCookiePerSale) {
+  this.name = name;
+  this.minHourlyCustomers = minHourlyCustomers;
+  this.maxHourlyCustomers = maxHourlyCustomers;
+  this.avgCookiePerSale = avgCookiePerSale;
+  this.totalCookiesSold = 0;
+  this.cookiesSoldInformationByHour = [];
+}
 
-      var hourNumber = i;
-      if (i > 12) {
-        hourNumber = i - 12;
-      }
-
-      var numberOfCustomerInCurrentHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers;
-
-      var numberOfCookiesSoldInCurrentHour = Math.round(numberOfCustomerInCurrentHour * this.avgCookiePerSale);
-      this.totalCookiesSold = this.totalCookiesSold + numberOfCookiesSoldInCurrentHour;
-
-      var textContentForCurrentHour =  hourNumber + morningEvening + numberOfCookiesSoldInCurrentHour + ' cookies';
-      this.cookiesSoldInformationByHour.push(textContentForCurrentHour);
+SalmonCookies.prototype.buildMarketSales = function () {
+  var openHour = 6;
+  var closeHour = 20;
+  for (var i = openHour; i <= closeHour; i++) { // loop through the hours
+    var morningEvening = 'pm: ';
+    if (i < 12) {
+      morningEvening = 'am: ';
     }
-  },
-  displayMarketSales: function() {
-    var salesDisplayArea = document.createElement('div');
-    salesDisplayArea.className += 'salesbox';
-    document.body.appendChild(salesDisplayArea);
 
-    var heading = document.createElement('h2');
-    heading.textContent = this.name;
-    salesDisplayArea.appendChild(heading);
+    var hourNumber = i;
+    if (i > 12) {
+      hourNumber = i - 12;
+    }
 
-    var list = document.createElement('u1');
-    salesDisplayArea.appendChild(list);
+    var numberOfCustomerInCurrentHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers;
 
-    this.cookiesSoldInformationByHour.forEach(function(textForAGivenHour) {
-      var item = document.createElement('li');
-      item.textContent = textForAGivenHour;
-      list.appendChild(item);
-    });
+    var numberOfCookiesSoldInCurrentHour = Math.round(numberOfCustomerInCurrentHour * this.avgCookiePerSale);
+    this.totalCookiesSold = this.totalCookiesSold + numberOfCookiesSoldInCurrentHour;
 
-    var total = document.createElement('li');
-
-    total.textContent = 'Total: ' + this.totalCookiesSold + ' cookies';
-    list.appendChild(total);
+    var textContentForCurrentHour = hourNumber + morningEvening + numberOfCookiesSoldInCurrentHour + ' cookies';
+    this.cookiesSoldInformationByHour.push(textContentForCurrentHour);
   }
 };
 
-firstAndPikeShop.buildMarketSales();
-firstAndPikeShop.displayMarketSales();
+SalmonCookies.prototype.displayMarketSales = function () {
+  var salesDisplayArea = document.createElement('div');
+  salesDisplayArea.className += 'salesbox';
+  document.body.appendChild(salesDisplayArea);
 
-var airport = {
-  name: 'SeaTac Airport',
-  minHourlyCustomers: 3,
-  maxHourlyCustomers: 24,
-  avgCookiePerSale: 1.2,
-  totalCookiesSold: 0,
-  cookiesSoldInformationByHour: [],
-  buildMarketSales: function() {
-    var openHour = 6;
-    var closeHour = 20;
-    for (var i = openHour; i <= closeHour; i++) { // loop through the hours
-      var morningEvening = 'pm: ';
-      if (i < 12) {
-        morningEvening = 'am: ';
-      }
+  var heading = document.createElement('h2');
+  heading.textContent = this.name;
+  salesDisplayArea.appendChild(heading);
 
-      var hourNumber = i;
-      if (i > 12) {
-        hourNumber = i - 12;
-      }
+  var list = document.createElement('ul');
+  salesDisplayArea.appendChild(list);
 
-      var numberOfCustomerInCurrentHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers;
-
-      var numberOfCookiesSoldInCurrentHour = Math.round(numberOfCustomerInCurrentHour * this.avgCookiePerSale);
-      this.totalCookiesSold = this.totalCookiesSold + numberOfCookiesSoldInCurrentHour;
-
-      var textContentForCurrentHour =  hourNumber + morningEvening + numberOfCookiesSoldInCurrentHour + ' cookies';
-      this.cookiesSoldInformationByHour.push(textContentForCurrentHour);
-    }
-  },
-  displayMarketSales: function() {
-    var salesDisplayArea = document.createElement('div');
-    salesDisplayArea.className += 'salesbox';
-    document.body.appendChild(salesDisplayArea);
-
-    var heading = document.createElement('h2');
-    heading.textContent = this.name;
-    salesDisplayArea.appendChild(heading);
-
-    var list = document.createElement('u1');
-    salesDisplayArea.appendChild(list);
-
-    this.cookiesSoldInformationByHour.forEach(function(textForAGivenHour) {
-      var item = document.createElement('li');
-      item.textContent = textForAGivenHour;
-      list.appendChild(item);
-    });
-
-    var total = document.createElement('li');
-
-    total.textContent = 'Total: ' + this.totalCookiesSold + ' cookies';
-    list.appendChild(total);
+  for (var i = 0; i < this.cookiesSoldInformationByHour.length; i++) {
+    var item = document.createElement('li');
+    item.textContent = this.cookiesSoldInformationByHour[i];
+    list.appendChild(item);
   }
+
+  // this.cookiesSoldInformationByHour.forEach(function (textForAGivenHour) {
+  //   var item = document.createElement('li');
+  //   item.textContent = textForAGivenHour;
+  //   list.appendChild(item);
+  // });
+
+
+  var total = document.createElement('li');
+  total.textContent = 'Total: ' + this.totalCookiesSold + ' cookies';
+  list.appendChild(total);
 };
 
+var firstAndPike = new SalmonCookies('1st and pike', 23, 65, 6.3);
+var airport = new SalmonCookies('SeaTac Airport', 3, 24, 1.2);
+var centerOfSeattle = new SalmonCookies('Seattle Center', 11, 38, 3.7);
+var capHill = new SalmonCookies('Capitol Hill', 20, 38, 2.3);
+var beach = new SalmonCookies('Alki', 2, 16, 4.6);
+
+firstAndPike.buildMarketSales();
 airport.buildMarketSales();
-airport.displayMarketSales();
-
-var centerOfSeattle = {
-  name: 'Seattle Center',
-  minHourlyCustomers: 11,
-  maxHourlyCustomers: 38,
-  avgCookiePerSale: 3.7,
-  totalCookiesSold: 0,
-  cookiesSoldInformationByHour: [],
-  buildMarketSales: function() {
-    var openHour = 6;
-    var closeHour = 20;
-    for (var i = openHour; i <= closeHour; i++) { // loop through the hours
-      var morningEvening = 'pm: ';
-      if (i < 12) {
-        morningEvening = 'am: ';
-      }
-
-      var hourNumber = i;
-      if (i > 12) {
-        hourNumber = i - 12;
-      }
-
-      var numberOfCustomerInCurrentHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers;
-
-      var numberOfCookiesSoldInCurrentHour = Math.round(numberOfCustomerInCurrentHour * this.avgCookiePerSale);
-      this.totalCookiesSold = this.totalCookiesSold + numberOfCookiesSoldInCurrentHour;
-
-      var textContentForCurrentHour =  hourNumber + morningEvening + numberOfCookiesSoldInCurrentHour + ' cookies';
-      this.cookiesSoldInformationByHour.push(textContentForCurrentHour);
-    }
-  },
-  displayMarketSales: function() {
-    var salesDisplayArea = document.createElement('div');
-    salesDisplayArea.className += 'salesbox';
-    document.body.appendChild(salesDisplayArea);
-
-    var heading = document.createElement('h2');
-    heading.textContent = this.name;
-    salesDisplayArea.appendChild(heading);
-
-    var list = document.createElement('u1');
-    salesDisplayArea.appendChild(list);
-
-    this.cookiesSoldInformationByHour.forEach(function(textForAGivenHour) {
-      var item = document.createElement('li');
-      item.textContent = textForAGivenHour;
-      list.appendChild(item);
-    });
-
-    var total = document.createElement('li');
-
-    total.textContent = 'Total: ' + this.totalCookiesSold + ' cookies';
-    list.appendChild(total);
-  }
-};
-
 centerOfSeattle.buildMarketSales();
-centerOfSeattle.displayMarketSales();
-
-var capHill = {
-  name: 'Capitol Hill',
-  minHourlyCustomers: 20,
-  maxHourlyCustomers: 38,
-  avgCookiePerSale: 2.3,
-  totalCookiesSold: 0,
-  cookiesSoldInformationByHour: [],
-  buildMarketSales: function() {
-    var openHour = 6;
-    var closeHour = 20;
-    for (var i = openHour; i <= closeHour; i++) { // loop through the hours
-      var morningEvening = 'pm: ';
-      if (i < 12) {
-        morningEvening = 'am: ';
-      }
-
-      var hourNumber = i;
-      if (i > 12) {
-        hourNumber = i - 12;
-      }
-
-      var numberOfCustomerInCurrentHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers;
-
-      var numberOfCookiesSoldInCurrentHour = Math.round(numberOfCustomerInCurrentHour * this.avgCookiePerSale);
-      this.totalCookiesSold = this.totalCookiesSold + numberOfCookiesSoldInCurrentHour;
-
-      var textContentForCurrentHour =  hourNumber + morningEvening + numberOfCookiesSoldInCurrentHour + ' cookies';
-      this.cookiesSoldInformationByHour.push(textContentForCurrentHour);
-    }
-  },
-  displayMarketSales: function() {
-    var salesDisplayArea = document.createElement('div');
-    salesDisplayArea.className += 'salesbox';
-    document.body.appendChild(salesDisplayArea);
-
-    var heading = document.createElement('h2');
-    heading.textContent = this.name;
-    salesDisplayArea.appendChild(heading);
-
-    var list = document.createElement('u1');
-    salesDisplayArea.appendChild(list);
-
-    this.cookiesSoldInformationByHour.forEach(function(textForAGivenHour) {
-      var item = document.createElement('li');
-      item.textContent = textForAGivenHour;
-      list.appendChild(item);
-    });
-
-    var total = document.createElement('li');
-
-    total.textContent = 'Total: ' + this.totalCookiesSold + ' cookies';
-    list.appendChild(total);
-  }
-};
-
 capHill.buildMarketSales();
-capHill.displayMarketSales();
-
-var beach = {
-  name: 'Alki',
-  minHourlyCustomers: 2,
-  maxHourlyCustomers: 16,
-  avgCookiePerSale: 4.6,
-  totalCookiesSold: 0,
-  cookiesSoldInformationByHour: [],
-  buildMarketSales: function() {
-    var openHour = 6;
-    var closeHour = 20;
-    for (var i = openHour; i <= closeHour; i++) { // loop through the hours
-      var morningEvening = 'pm: ';
-      if (i < 12) {
-        morningEvening = 'am: ';
-      }
-
-      var hourNumber = i;
-      if (i > 12) {
-        hourNumber = i - 12;
-      }
-
-      var numberOfCustomerInCurrentHour = Math.floor(Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1)) + this.minHourlyCustomers;
-
-      var numberOfCookiesSoldInCurrentHour = Math.round(numberOfCustomerInCurrentHour * this.avgCookiePerSale);
-      this.totalCookiesSold = this.totalCookiesSold + numberOfCookiesSoldInCurrentHour;
-
-      var textContentForCurrentHour =  hourNumber + morningEvening + numberOfCookiesSoldInCurrentHour + ' cookies';
-      this.cookiesSoldInformationByHour.push(textContentForCurrentHour);
-    }
-  },
-  displayMarketSales: function() {
-    var salesDisplayArea = document.createElement('div');
-    salesDisplayArea.className += 'salesbox';
-    document.body.appendChild(salesDisplayArea);
-
-    var heading = document.createElement('h2');
-    heading.textContent = this.name;
-    salesDisplayArea.appendChild(heading);
-
-    var list = document.createElement('u1');
-    salesDisplayArea.appendChild(list);
-
-    this.cookiesSoldInformationByHour.forEach(function(textForAGivenHour) {
-      var item = document.createElement('li');
-      item.textContent = textForAGivenHour;
-      list.appendChild(item);
-    });
-
-    var total = document.createElement('li');
-
-    total.textContent = 'Total: ' + this.totalCookiesSold + ' cookies';
-    list.appendChild(total);
-  }
-};
-
 beach.buildMarketSales();
-beach.displayMarketSales();
 
+firstAndPike.displayMarketSales();
+airport.displayMarketSales();
+centerOfSeattle.displayMarketSales();
+capHill.displayMarketSales();
+beach.displayMarketSales();
